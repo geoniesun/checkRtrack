8771a8923a47dab8948773d6b1c2a2b1b2ddc7b5
geoniesun <leoniesonntag26@gmail.com>
2024-06-06 18:00:16 +0200

    adding man

--- R/checkPolygon.R
+++ R/checkPolygon.R
@@ -1,3 +1,23 @@
+
+
+#' Generates a polygon around the input lines as wide as the detected width of the track
+#'
+#' @param dsm Digital Surface Model raster file as '.tif'.
+#' @param tracks Digitized tracks as GeoPackage '.gpkg'.
+#' @param export If 'TRUE' the GeoPackage will be exported to your wd.
+#' @param dist_cross Distance between each crossprofile in meter. Defaults to '1'.
+#' @param profile_length Length of the crossprofile in meter. Defaults to '1'.
+#' @param dist_cross_points Distance of the points on the crossprofile in meter. Defaults to '0.05'.
+#' @param st_dev Minimum standarddeviation of crossprofile dsm-value 'z'. Acts as a filter. Defaults to '0.06'.
+#'
+#' @return a buffer around the input lines representing the calculated width of the tracks. It includes a column with the width in meters.
+#' @export
+#'
+#' @examples
+#' polygon_as_width <- checkPolygon(dsm , tracks, export = FALSE, dist_cross = 1,profile_length = 1, dist_cross_points = 0.05,st_dev = 0.06
+#' polygon_as_width
+
+
 checkPolygon <- function(dsm , tracks, export = FALSE, dist_cross = 1,
                        profile_length = 1, dist_cross_points = 0.05,
                        st_dev = 0.06) {
@@ -6,8 +26,8 @@ checkPolygon <- function(dsm , tracks, export = FALSE, dist_cross = 1,
   #extract the width of mean values for segments
 line_width_mean <- checkWidth(dsm, tracks, export, plot, dist_cross, profile_length, dist_cross_points, st_dev)
 
-
-  polygons <- st_buffer(line_width_mean, dist = line_width_mean$width)
+halfdist <- (line_width_mean$width)/2
+  polygons <- st_buffer(line_width_mean, dist = halfdist)
 
   # export the points as GeoPackage
   if(isTRUE(export)) {
